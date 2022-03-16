d772fa11dca49fa1fa114112fb6f205c82b23a21
Sam Brown <sam.brown@mongodb.com>
2022-03-16 12:25:18 +0000

    Undo committing out configuration

--- 1_csv_to_mongodb/src/main/java/com/sbrown52/examples/mongodbspringbatch/MetadataDatabaseConfiguration.java
+++ 1_csv_to_mongodb/src/main/java/com/sbrown52/examples/mongodbspringbatch/MetadataDatabaseConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import javax.sql.DataSource;
 
-//@Configuration
+@Configuration
 public class MetadataDatabaseConfiguration {
 
     @Value("${metadata.database.driver}")
